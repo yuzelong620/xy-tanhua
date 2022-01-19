3474ca6fd2279344a4e89fcb89ca8b35cc656548
yzl <382981935@qq.com>
2022-01-19 18:59:05 +0800

    注释

--- xy-tanhua-sso/src/main/java/com/tanhua/sso/mapper/UserMapper.java
+++ xy-tanhua-sso/src/main/java/com/tanhua/sso/mapper/UserMapper.java
@@ -2,7 +2,7 @@ package com.tanhua.sso.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tanhua.sso.pojo.User;
-
+//注释
 public interface UserMapper extends BaseMapper<User> {
 
 }
